402d0594ede78b522a4f0ddeafd96d81a5a30fb6
00sanoj00 <00sanoj00@gmail.com>
2019-07-12 23:52:25 +0530

    Main update
    full update

---
+++ Form1.vb
@@ -0,0 +1,81 @@
+﻿Public Class Form1
+    Dim codes As String = "color 7"
+    Private Sub ViButton1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ViButton1.Click
+        Process.Start("https://www.widane.com/")
+        End
+    End Sub
+
+    Private Sub Form1_Leave(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Leave
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+        If ComboBox1.Text.Equals("නිල් පාට") Then
+            TextBox1.ForeColor = Color.Blue
+        ElseIf ComboBox1.Text.Equals("කොළ පාට") Then
+            TextBox1.ForeColor = Color.Green
+        ElseIf ComboBox1.Text.Equals("ලා නිල් පාට") Then
+            TextBox1.ForeColor = Color.Cyan
+        ElseIf ComboBox1.Text.Equals("රතු පාට") Then
+            TextBox1.ForeColor = Color.Red
+        ElseIf ComboBox1.Text.Equals("දම් පාට") Then
+            TextBox1.ForeColor = Color.Purple
+        ElseIf ComboBox1.Text.Equals("කහ පාට") Then
+            TextBox1.ForeColor = Color.Yellow
+        ElseIf ComboBox1.Text.Equals("සුදු පාට") Then
+            TextBox1.ForeColor = Color.Wheat
+        ElseIf ComboBox1.Text.Equals("අළු පාට") Then
+            TextBox1.ForeColor = Color.Gray
+        ElseIf ComboBox1.Text.Equals("තද නිල් පාට") Then
+            TextBox1.ForeColor = Color.Navy
+        End If
+
+    End Sub
+
+    Private Sub ViButton2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ViButton2.Click
+        If ComboBox1.Text.Equals("") Then
+            MessageBox.Show("උඩ ටික හරියට පුරවලා ඉදන් බන්")
+        Else
+            If ComboBox1.Text.Equals("නිල් පාට") Then
+                codes = "color 1"
+            ElseIf ComboBox1.Text.Equals("කොළ පාට") Then
+                codes = "color 2"
+            ElseIf ComboBox1.Text.Equals("ලා නිල් පාට") Then
+                codes = "color 3"
+            ElseIf ComboBox1.Text.Equals("රතු පාට") Then
+                codes = "color 4"
+            ElseIf ComboBox1.Text.Equals("දම් පාට") Then
+                codes = "color 5"
+            ElseIf ComboBox1.Text.Equals("කහ පාට") Then
+                codes = "color 6"
+            ElseIf ComboBox1.Text.Equals("සුදු පාට") Then
+                codes = "color 7"
+            ElseIf ComboBox1.Text.Equals("අළු පාට") Then
+                codes = "color 8"
+            ElseIf ComboBox1.Text.Equals("තද නිල් පාට") Then
+                codes = "color 9"
+            End If
+            MyUtilities.RunCommandCom(codes, "", True)
+        End If
+
+    End Sub
+    Public Class MyUtilities
+        Shared Sub RunCommandCom(ByVal command As String, ByVal arguments As String, ByVal permanent As Boolean)
+            Dim p As Process = New Process()
+            Dim pi As ProcessStartInfo = New ProcessStartInfo()
+            pi.Arguments = " " + If(permanent = True, "/K", "/C") + " " + command + " " + arguments
+            pi.FileName = "cmd.exe"
+            p.StartInfo = pi
+            p.Start()
+        End Sub
+    End Class
+
+    Private Sub PictureBox1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox1.Click
+        Process.Start("https://www.facebook.com/sanoj.jayathilaka1")
+    End Sub
+End Class
---
+++ Opencmd.bat/Form1.Designer.vb
@@ -0,0 +1,194 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ViTheme1 = New Opencmd.bat.VITheme
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.ViButton2 = New Opencmd.bat.VIButton
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox
+        Me.ViButton1 = New Opencmd.bat.VIButton
+        Me.ViTheme1.SuspendLayout()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ViTheme1
+        '
+        Me.ViTheme1.BorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.ViTheme1.Colors = New Opencmd.bat.Bloom(-1) {}
+        Me.ViTheme1.Controls.Add(Me.PictureBox3)
+        Me.ViTheme1.Controls.Add(Me.PictureBox2)
+        Me.ViTheme1.Controls.Add(Me.PictureBox1)
+        Me.ViTheme1.Controls.Add(Me.TextBox1)
+        Me.ViTheme1.Controls.Add(Me.ViButton2)
+        Me.ViTheme1.Controls.Add(Me.Label1)
+        Me.ViTheme1.Controls.Add(Me.ComboBox1)
+        Me.ViTheme1.Controls.Add(Me.ViButton1)
+        Me.ViTheme1.Customization = ""
+        Me.ViTheme1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ViTheme1.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.ViTheme1.Image = Nothing
+        Me.ViTheme1.Location = New System.Drawing.Point(0, 0)
+        Me.ViTheme1.Movable = True
+        Me.ViTheme1.Name = "ViTheme1"
+        Me.ViTheme1.NoRounding = False
+        Me.ViTheme1.Sizable = False
+        Me.ViTheme1.Size = New System.Drawing.Size(214, 208)
+        Me.ViTheme1.SmartBounds = True
+        Me.ViTheme1.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.ViTheme1.TabIndex = 0
+        Me.ViTheme1.Text = "ⓞⓟⓔⓝ ⓒⓜⓓ"
+        Me.ViTheme1.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.ViTheme1.Transparent = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(89, 39)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(42, 22)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 10
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(134, 39)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(42, 22)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 9
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(180, 40)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(22, 22)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 8
+        Me.PictureBox1.TabStop = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.Black
+        Me.TextBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.TextBox1.Location = New System.Drawing.Point(15, 132)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(187, 64)
+        Me.TextBox1.TabIndex = 7
+        Me.TextBox1.Text = "===================" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "=                     කලේ                      =" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "=         " & _
+            "   සනෝජ්   ප්‍රභාත්            =" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "==================="
+        '
+        'ViButton2
+        '
+        Me.ViButton2.Colors = New Opencmd.bat.Bloom(-1) {}
+        Me.ViButton2.Customization = ""
+        Me.ViButton2.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.ViButton2.Image = Nothing
+        Me.ViButton2.Location = New System.Drawing.Point(15, 101)
+        Me.ViButton2.Name = "ViButton2"
+        Me.ViButton2.NoRounding = False
+        Me.ViButton2.Size = New System.Drawing.Size(187, 25)
+        Me.ViButton2.TabIndex = 6
+        Me.ViButton2.Text = "ආරම්බ කරන්ණ"
+        Me.ViButton2.Transparent = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(9, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "අකුරුවල පාට"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"නිල් පාට", "කොළ පාට", "ලා නිල් පාට", "රතු පාට", "දම් පාට", "කහ පාට", "සුදු පාට", "අළු පාට", "තද නිල් පාට"})
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 68)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(190, 21)
+        Me.ComboBox1.TabIndex = 2
+        '
+        'ViButton1
+        '
+        Me.ViButton1.Colors = New Opencmd.bat.Bloom(-1) {}
+        Me.ViButton1.Customization = ""
+        Me.ViButton1.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.ViButton1.Image = Nothing
+        Me.ViButton1.Location = New System.Drawing.Point(180, 3)
+        Me.ViButton1.Name = "ViButton1"
+        Me.ViButton1.NoRounding = False
+        Me.ViButton1.Size = New System.Drawing.Size(22, 25)
+        Me.ViButton1.TabIndex = 0
+        Me.ViButton1.Text = "X"
+        Me.ViButton1.Transparent = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(214, 208)
+        Me.Controls.Add(Me.ViTheme1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.ViTheme1.ResumeLayout(False)
+        Me.ViTheme1.PerformLayout()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ViTheme1 As Opencmd.bat.VITheme
+    Friend WithEvents ViButton1 As Opencmd.bat.VIButton
+    Friend WithEvents ViButton2 As Opencmd.bat.VIButton
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+
+End Class
